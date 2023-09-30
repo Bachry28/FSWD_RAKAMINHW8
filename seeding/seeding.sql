44241af4e0caa0ceba07416b4bc9ae57ba066af1
Bachry28 <143858195+Bachry28@users.noreply.github.com>
2023-09-30 18:33:41 +0700

    Delete seeding/seeding.sql
    seeding sql sudah berada di readme

--- seeding/seeding.sql
+++
@@ -1,5 +0,0 @@
-
-
-INSERT INTO actor (actor_id, first_name, last_name, last_update)
-VALUES (202, 'karlk', 'wise', CURRENT_TIMESTAMP), (203, 'ilomi', 'quert', CURRENT_TIMESTAMP), 
-(204, 'andri', 'swezx', CURRENT_TIMESTAMP), (205, 'malyon', 'adict', CURRENT_TIMESTAMP)
